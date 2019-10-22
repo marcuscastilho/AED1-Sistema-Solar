bad90c9c5f064afcf1af56722aa85df2faa16806
Marcus Castilho <marcus@iconexlog.com.br>
2019-10-21 23:16:48 -0300

    Atualizando classes

---
+++ planetas.cs
@@ -0,0 +1,19 @@
+class planeta
+{
+    //ATRIBUTOS
+    private float massa, diametro, dist_sol, pos_sistSolar;
+    private string [] satelites = new string [];
+    
+    //CONSTRUTORES
+    public planeta(float m, float d, float ds, float ps){
+        massa = m;
+        diametro = d;
+        dist_sol = ds;
+        pos_sistSolar = ps;
+        
+    }
+
+    //MÉTODOS
+    //Nº de satelites
+    
+}
---
+++ satelites_artificiais.cs
@@ -0,0 +1,17 @@
+class planetaAnao
+{
+    //ATRIBUTOS
+    private float massa, diametro, dist_sol, pos_sistSolar;
+    private string [] satelites = new string [];
+
+    //CONSTRUTORES
+    public planetaAnao(float m, float d, float ds){
+        massa = m;
+        diametro = d;
+        dist_sol = ds;
+        
+    }
+
+    //MÉTODOS
+    
+}
